3a3e45e37ef775a5bd94cd9485c98d4738b8575c
narharikale <n7kale@gmail.com>
2025-03-23 12:14:23 +0530

    feat: add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  return <>HEllo world</>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
